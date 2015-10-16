bc543e63093cb964ad97824e4a54cac45f88dd0d
pietro.marchi <pietro.marchi@MW7JF3QU9N28UJ.dir.svc.accenture.com>
2015-10-16 15:48:57 -0300

    commits de template

--- GerenciadorTemakeria/src/main/java/com/temakeria/dao/mesa/MesaDAOImpl.java
+++ GerenciadorTemakeria/src/main/java/com/temakeria/dao/mesa/MesaDAOImpl.java
@@ -1,7 +1,5 @@
 package com.temakeria.dao.mesa;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.temakeria.dao.GenericDAO;
@@ -9,6 +7,6 @@ import com.temakeria.model.mesa.Mesa;
 
 @Repository
 public class MesaDAOImpl extends GenericDAO<Mesa> implements MesaDAO {
-	private static final Logger logger = LoggerFactory.getLogger(MesaDAOImpl.class);
+	//private static final Logger logger = LoggerFactory.getLogger(MesaDAOImpl.class);
 	
 }
